c25064ae81df0fbd0b8d6c8a06112a4c6a427b5b
Olivier THIERRY <othierry@asi.fr>
2020-12-09 18:04:30 +0100

    Ajout de Bootstrap / Reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          <Button color="danger">Danger !</Button>
+        </p>
       </header>
     </div>
   );
