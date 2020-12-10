13caa6ada46e9a536e5f4e9288be0d2a2611acb3
Olivier THIERRY <othierry@asi.fr>
2020-12-10 12:22:25 +0100

    Création du composant PandasListView

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'reactstrap';
+import PandasListView from './views/PandasListView';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          <Button color="danger">Danger !</Button>
-        </p>
-      </header>
-    </div>
-  );
+  return <PandasListView />;
 }
 
 export default App;
---
+++ src/views/PandasListView/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PandasList from '../../components/PandasList';
+import pandas from '../../mocks/pandas.json';
+
+const handlePress = (id: string) => {
+  alert(`Panda ${id} was pressed`);
+};
+
+const PandasListView = () => (
+  <PandasList pandas={pandas} onPress={handlePress} />
+);
+
+export default PandasListView;
