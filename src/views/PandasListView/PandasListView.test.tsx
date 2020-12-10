e85d585cc544cbac48842dbdeb0b2609e299affc
Olivier THIERRY <othierry@asi.fr>
2020-12-10 18:09:57 +0100

    Mise à jour de PandasListView

--- src/views/PandasListView/PandasListView.test.tsx
+++ src/views/PandasListView/PandasListView.test.tsx
@@ -1,9 +1,59 @@
-import { render, screen } from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import React from 'react';
+import pandas from '../../mocks/pandas.json';
 import PandasListView from './';
 
-test('renders a list of pandas', () => {
-  render(<PandasListView />);
-  const pandaElement = screen.getByText(/Yuan Meng/i);
-  expect(pandaElement).toBeInTheDocument();
+const axiosMock = new MockAdapter(axios);
+
+describe('PandasListView', () => {
+  afterEach(() => {
+    axiosMock.reset();
+  });
+
+  test('should render a list of pandas', async () => {
+    axiosMock.onGet('http://localhost:3004/pandas').reply(200, pandas);
+
+    const { getByText, findAllByRole } = render(<PandasListView />);
+
+    // Should display a loading indicator
+
+    const loadingElement = getByText(/Loading.../i);
+    expect(loadingElement).toBeInTheDocument();
+
+    // The loading indicator should disappear
+
+    await waitForElementToBeRemoved(() => getByText(/Loading.../i));
+    expect(loadingElement).not.toBeInTheDocument();
+
+    // Should display one list with a listitem for every panda
+
+    const listElements = await findAllByRole('list');
+    expect(listElements.length).toBe(1);
+
+    const itemElements = await findAllByRole('listitem');
+    expect(itemElements.length).toBe(pandas.length);
+  });
+
+  test('should fail to load pandas', async () => {
+    axiosMock.onGet('http://localhost:3004/pandas').networkError();
+
+    const { getByText } = render(<PandasListView />);
+
+    // Should display a loading indicator
+
+    const loadingElement = getByText(/Loading.../i);
+    expect(loadingElement).toBeInTheDocument();
+
+    // The loading indicator should disappear
+
+    await waitForElementToBeRemoved(() => getByText(/Loading.../i));
+    expect(loadingElement).not.toBeInTheDocument();
+
+    // Should display an error message
+
+    const errorElement = getByText(/Network Error/i);
+    expect(errorElement).toBeInTheDocument();
+  });
 });
