14d8d7027ca1f4cb866fe9a7b73daa82aec12d7a
Olivier THIERRY <othierry@asi.fr>
2020-12-10 18:09:57 +0100

    Mise à jour de PandasListView

--- src/views/PandasListView/index.tsx
+++ src/views/PandasListView/index.tsx
@@ -1,42 +1,19 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Spinner } from 'reactstrap';
 import PandasList from '../../components/PandasList';
-import { Panda } from '../../types/Panda';
+import usePandas from '../../hooks/usePandas';
 
 const handlePress = (id: string) => {
   alert(`Panda ${id} was pressed`);
 };
 
 const PandasListView = () => {
-  const [pandas, setPandas] = useState<Panda[] | undefined>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | undefined>();
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      axios
-        .get('http://localhost:3004/pandas')
-        .then((result) => {
-          setPandas(result.data);
-          setError('');
-        })
-        .catch((error) => {
-          setError(error.message);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }, 2000);
-  }, []);
-
+  const { data, isLoading, error } = usePandas();
   return (
     <>
       {isLoading && <Spinner />}
       {error && <p>{error}</p>}
-      {!isLoading && pandas && (
-        <PandasList pandas={pandas} onPress={handlePress} />
-      )}
+      {!isLoading && data && <PandasList pandas={data} onPress={handlePress} />}
     </>
   );
 };
