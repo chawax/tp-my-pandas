6efad8f2eddc6ec5e1735d966d3b67d076c72384
Olivier THIERRY <othierry@asi.fr>
2020-12-16 15:47:49 +0100

    Ajout de React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import type { ReactNode } from 'react';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import PandasListView from './views/PandasListView';
 
+const queryClient = new QueryClient();
+const ReactQueryWrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
 function App() {
-  return <PandasListView />;
+  return (
+    <ReactQueryWrapper>
+      <PandasListView />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ReactQueryWrapper>
+  );
 }
 
 export default App;
