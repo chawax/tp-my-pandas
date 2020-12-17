64b2ebbc7c8c9e878d4bbbbe9148c010a44d0e54
Olivier THIERRY <othierry@asi.fr>
2020-12-17 10:57:37 +0100

    Mise en place de la navigation avec React Router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 const Header: React.FC = () => {
   return (
     <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/">
-        <h1>My pandas</h1>
-      </NavbarBrand>
+      <Link to="/">
+        <NavbarBrand>
+          <h1>My pandas</h1>
+        </NavbarBrand>
+      </Link>
     </Navbar>
   );
 };
--- src/views/PandasListView/index.tsx
+++ src/views/PandasListView/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Spinner } from 'reactstrap';
 import ErrorAndRetry from '../../components/ErrorAndRetry';
 import PandasList from '../../components/PandasList';
 import usePandas from '../../hooks/usePandas';
 
-const handlePress = (id: string) => {
-  alert(`Panda ${id} was pressed`);
-};
-
 const PandasListView = () => {
+  const history = useHistory();
+
+  const handlePress = useCallback(
+    (id: string) => {
+      history.push(`/pandas/${id}`);
+    },
+    [history],
+  );
+
   const { isLoading, isSuccess, data, error, refetch } = usePandas();
   return (
     <>
