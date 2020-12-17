ae4ffb7e8006058dd99ce3553b1238121515be77
Olivier THIERRY <othierry@asi.fr>
2020-12-17 20:42:50 +0100

    Changement de la langue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
 const Header: React.FC = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const handleChangeLanguage = useCallback(
+    (language: string) => {
+      i18n.changeLanguage(language);
+    },
+    [i18n],
+  );
+
   return (
-    <Navbar color="dark" dark expand="md">
+    <Navbar color="dark" expand="md" className="d-flex justify-content-between">
       <Link to="/">
         <NavbarBrand>
           <h1>{t('app.title')}</h1>
         </NavbarBrand>
       </Link>
+      <Nav style={{ color: 'white' }}>
+        <span
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            handleChangeLanguage('fr');
+          }}
+        >
+          FR
+        </span>
+        &nbsp;|&nbsp;
+        <span
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            handleChangeLanguage('en');
+          }}
+        >
+          EN
+        </span>
+      </Nav>
     </Navbar>
   );
 };
