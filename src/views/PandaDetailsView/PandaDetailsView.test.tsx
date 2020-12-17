e9a560ad24b5f96961bd18a4c0db7c77da400369
Olivier THIERRY <othierry@asi.fr>
2020-12-17 17:17:33 +0100

    Une version qui marche, mais c'est complexe !

--- src/views/PandaDetailsView/PandaDetailsView.test.tsx
+++ src/views/PandaDetailsView/PandaDetailsView.test.tsx
@@ -1,16 +1,14 @@
-import {
-  findByRole,
-  getByRole,
-  render,
-  waitForElementToBeRemoved,
-} from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import type { ReactNode } from 'react';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { MemoryRouter, Route } from 'react-router-dom';
 import pandas from '../../mocks/pandas.json';
 import PandaDetailsView from './';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,6 +23,30 @@ const ReactQueryWrapper = ({ children }: { children: ReactNode }) => (
 
 const axiosMock = new MockAdapter(axios);
 
+// https://testing-library.com/docs/example-react-router
+
+function renderWithRouter(
+  //@ts-ignore
+  ui,
+  {
+    path = '/',
+    //@ts-ignore
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {},
+) {
+  return {
+    ...render(
+      <ReactQueryWrapper>
+        <Router history={history}>
+          <Route path={path} component={ui} />
+        </Router>
+      </ReactQueryWrapper>,
+    ),
+    history,
+  };
+}
+
 describe('PandaDetailsView', () => {
   afterEach(() => {
     axiosMock.reset();
@@ -34,11 +56,34 @@ describe('PandaDetailsView', () => {
   test('should render the details of the panda', async () => {
     axiosMock.onGet('http://localhost:3004/pandas/1').reply(200, pandas[0]);
 
-    const { getByText, findAllByRole, findByRole, getByRole } = render(
+    /*
+    const { getByText, getByRole } = render(
       <ReactQueryWrapper>
-        <PandaDetailsView />
+      <Router history={history}>
+      <Route path="/pandas/1" component={PandaDetailsView} />
+      </Router>
+      </ReactQueryWrapper>,
+      );
+      */
+
+    const { getByText, getByRole } = renderWithRouter(PandaDetailsView, {
+      route: '/pandas/1',
+      path: '/pandas/:id',
+    });
+
+    /*
+    const history = createMemoryHistory({
+      initialEntries: ['/', '/pandas/1'],
+    });
+    const path = '/pandas/:id';
+    const { getByText, getByRole } = render(
+      <ReactQueryWrapper>
+        <Router history={history}>
+          <Route path={path} component={PandaDetailsView} />
+        </Router>
       </ReactQueryWrapper>,
     );
+    */
 
     // Should display a loading indicator
 
@@ -67,11 +112,20 @@ describe('PandaDetailsView', () => {
   test('should fail to load the details of the panda', async () => {
     axiosMock.onGet('http://localhost:3004/pandas/1').networkError();
 
+    const { getByText } = renderWithRouter(PandaDetailsView, {
+      route: '/pandas/1',
+      path: '/pandas/:id',
+    });
+
+    /*
     const { getByText } = render(
       <ReactQueryWrapper>
-        <PandaDetailsView />
+        <Router history={history}>
+          <PandaDetailsView />
+        </Router>
       </ReactQueryWrapper>,
     );
+    */
 
     // Should display a loading indicator
 
